4050038e3c3863ba3d58b311d895ef48b7be7af6
Minwoo Jeong <jmw95217@gmail.com>
2019-02-21 18:26:13 +0900

    change the format of tx position table to handle duplicated locus

--- src/Canonical/01_make_splice_table.py
+++ src/Canonical/01_make_splice_table.py
@@ -18,7 +18,7 @@ os.makedirs(out_dir, exist_ok=True)
 regex_chr = re.compile('^chr([0-9]{1,2}|[XY])$')
 
 gene_table = {}  # mapping route: chrom -> strand -> gene_name -> transcript_name -> [exon_start_list, exon_end_list]
-tx_to_position = {}  # key: transcript name, value: (chrom, strand, start, end)
+tx_position_table = {}  # mapping route: chrom -> strand -> transcript_name -> [tx_start, tx_end]
 
 with open(gencode_path, 'r') as infile:
     for line in infile:
@@ -50,9 +50,11 @@ with open(gencode_path, 'r') as infile:
 
             if chrom not in gene_table:
                 gene_table[chrom] = {}
+                tx_position_table[chrom] = {}
 
             if strand not in gene_table[chrom]:
                 gene_table[chrom][strand] = {}
+                tx_position_table[chrom][strand] = {}
 
             gene_table[chrom][strand][gene_name] = {}
 
@@ -60,7 +62,7 @@ with open(gencode_path, 'r') as infile:
             gene_name = info_table['gene_name']
             tx_name = info_table['transcript_name']
             gene_table[chrom][strand][gene_name][tx_name] = []
-            tx_to_position[tx_name] = (chrom, strand, start, end)
+            tx_position_table[chrom][strand][tx_name] = (start, end)
 
         elif entry_type == 'exon':
             gene_name = info_table['gene_name']
@@ -74,7 +76,7 @@ with open(out_file_path, 'w') as outfile:
                 tx_num = 0
 
                 for tx_name in gene_table[chrom][strand][gene_name]:
-                    _, _, tx_start, tx_end = tx_to_position[tx_name]
+                    tx_start, tx_end = tx_position_table[chrom][strand][tx_name]
                     exons = gene_table[chrom][strand][gene_name][tx_name]
                     exons.sort()
                     exon_cnt = len(exons)
